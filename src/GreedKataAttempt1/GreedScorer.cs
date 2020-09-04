bf6a743f641f6e73323ef66161f640b9a009457c
Steve Smith <steve@kentsmiths.com>
2020-09-04 13:11:13 -0400

    Add 5s test and refactor tests

--- src/GreedKataAttempt1/GreedScorer.cs
+++ src/GreedKataAttempt1/GreedScorer.cs
@@ -1,10 +1,22 @@
-﻿namespace GreedKataAttempt1
+﻿using System.Linq;
+
+namespace GreedKataAttempt1
 {
     public class GreedScorer
     {
         public int ScoreRoll(params int[] dieValues)
         {
-            return 0;
+            if (dieValues.Count(i => i == 5) == 1)
+            {
+                return 50;
+            }
+            if (!dieValues.Any(i => i == 1)) return 0;
+
+            if (dieValues.Count(i => i == 1) == 1)
+            {
+                return 100;
+            }
+            return 200;
         }
     }
 }
--- src/GreedKataAttempt1/GreedScorer_ScoreRoll.cs
+++ src/GreedKataAttempt1/GreedScorer_ScoreRoll.cs
@@ -6,23 +6,40 @@ namespace GreedKataAttempt1
 {
     public class GreedScorer_ScoreRoll
     {
+        private GreedScorer _scorer = new GreedScorer();
+
         [Fact]
         public void Returns100GivenSingleOne()
         {
-            var scorer = new GreedScorer();
-
-            var result = scorer.ScoreRoll(1);
+            var result = _scorer.ScoreRoll(1);
 
             result.Should().Be(100);
         }
         [Fact]
+        public void Returns50GivenSingleFive()
+        {
+            var result = _scorer.ScoreRoll(5);
+
+            result.Should().Be(50);
+        }
+        [Fact]
         public void Returns200GivenTwoOnes()
         {
-            var scorer = new GreedScorer();
-
-            var result = scorer.ScoreRoll(1,1);
+            var result = _scorer.ScoreRoll(1, 1);
 
             result.Should().Be(200);
+        }
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(6)]
+        public void Returns0GivenSingleWorthlessValue(int val)
+        {
+            var result = _scorer.ScoreRoll(val);
+
+            result.Should().Be(0);
         }
     }
 }
